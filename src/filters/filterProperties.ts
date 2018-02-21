9ac0a50ea8da2aafa83b18a699cd10d9968079db
jy95 <jacques.yakoub@gmail.com>
2018-02-21 23:14:38 +0100

    :zap: Perf: Rewritten filter functions

--- src/filters/filterProperties.ts
+++ src/filters/filterProperties.ts
@@ -5,7 +5,7 @@
 import {
     filterDefaultBooleanProperties,
     filterByBoolean,
-    excludeDefaultBooleanProperties,
+    meetBooleanSpec,
 } from './filterBooleanProperty';
 
 /**
@@ -13,66 +13,79 @@ import {
  */
 import {
     convertToValidExpression,
-    excludeDefaultNumberProperties,
     filterDefaultNumberProperties,
-    filterByNumber,
+    filterByNumber, meetNumberSpec,
 } from './filterNumberProperty';
 
 /**
  * String properties filter
  */
 import {
-    excludeDefaultStringProperties,
-    filterDefaultStringProperties,
-    filterByString,
+    filterByString, filterDefaultStringProperties, meetStringSpec,
 } from './filterStringProperty';
 import MediaScan from "../declaration";
 
-/**
- * Handle searchParameters provided by user to maps
- * @param {searchParameters} searchParameters - search parameters.
- * an object that contains mapped properties for search
- */
 function mapProperties(searchParameters: MediaScan.SearchParameters): {
     booleanFieldsSearchMap: Map<string, boolean>, numberFieldsSearchMap: Map<string, MediaScan.NumberExpressionObject>,
-    stringFieldsSearchMap: Map<string, string | string[]> } {
+    stringFieldsSearchMap: Map<string, string | string[]>
+} {
 
-    // organize search based on field type : boolean - string - number
-    const booleanFieldsSearchMap = filterDefaultBooleanProperties(searchParameters);
-    let leftSearchParameters = excludeDefaultBooleanProperties(searchParameters);
-
-    const numberFieldsSearchMap = filterDefaultNumberProperties(leftSearchParameters);
-    leftSearchParameters = excludeDefaultNumberProperties(leftSearchParameters);
-
-    const stringFieldsSearchMap = filterDefaultStringProperties(leftSearchParameters);
-    leftSearchParameters = excludeDefaultStringProperties(leftSearchParameters);
-
-    let {additionalProperties} = leftSearchParameters;
+    // organize search based on field type : boolean - string - number . Now optimized by Me XD
     // add the optional new properties , optionally provided by user
-    /* istanbul ignore else */
-    if (additionalProperties !== undefined) {
+    let additionalProperties = (searchParameters.additionalProperties === undefined) ? [] : searchParameters.additionalProperties;
+    const booleanFieldsSearchMap = new Map(
+        <[string, boolean][]>
         additionalProperties
             .filter(newProperty => newProperty.type === 'boolean' as MediaScan.AdditionalPropertiesType.BOOLEAN)
-            .forEach((newProperty) => {
-                booleanFieldsSearchMap.set(newProperty.name, newProperty.value as boolean);
-            });
+            .reduce((sub_acc, {name, value}) => {
+                /* istanbul ignore else */
+                if ( meetBooleanSpec(value) )
+                    sub_acc.push([name, value]);
+                return sub_acc;
+            }, [])
+            .concat(
+                filterDefaultBooleanProperties(searchParameters)
+            )
+    );
 
+    const numberFieldsSearchMap = new Map(
+        <[string, MediaScan.NumberExpressionObject][]>
         additionalProperties
             .filter(newProperty => newProperty.type === 'number' as MediaScan.AdditionalPropertiesType.NUMBER)
-            .forEach((newProperty) => {
-                let expression = convertToValidExpression(newProperty.value as string|number);
+            .reduce((sub_acc, {name, value}) => {
                 /* istanbul ignore else */
-                if (expression !== undefined) {
-                    numberFieldsSearchMap.set(newProperty.name, expression);
-                }
-            });
+                if (meetNumberSpec(value))
+                    sub_acc.push([name, value]);
+                return sub_acc;
+            }, [])
+            .concat(
+                filterDefaultNumberProperties(searchParameters)
+            )
+            .map( ([name, value]) => [name,convertToValidExpression(value)] )
+            // just to remove the invalid expression from result
+            .filter( ([name, value]) => value !== undefined)
+    );
 
-        additionalProperties
-            .filter(newProperty => newProperty.type === 'string' as MediaScan.AdditionalPropertiesType.STRING)
-            .forEach((newProperty) => {
-                stringFieldsSearchMap.set(newProperty.name, [...newProperty.value]);
-            });
-    }
+    const stringFieldsSearchMap = new Map(
+        <[string, string|string[] ][]>
+            additionalProperties
+                .filter(newProperty => newProperty.type === 'string' as MediaScan.AdditionalPropertiesType.STRING)
+                .reduce( (sub_acc, {name, value} ) => {
+                    /* istanbul ignore else */
+                    if (meetStringSpec(value))
+                        sub_acc.push([name, value ]);
+                    return sub_acc;
+                }, [])
+                .concat(
+                    filterDefaultStringProperties(searchParameters)
+                        .reduce( (sub_acc, [name, value] ) => {
+                            /* istanbul ignore else */
+                            if (meetStringSpec(value))
+                                sub_acc.push([name, value ]);
+                            return sub_acc;
+                        }, [])
+                )
+    );
 
     return {
         booleanFieldsSearchMap,
@@ -91,7 +104,7 @@ export function filterMoviesByProperties(searchParameters: MediaScan.SearchParam
         = [booleanFieldsSearchMap, stringFieldsSearchMap, numberFieldsSearchMap];
     let result = allMovies;
     [filterByBoolean, filterByString, filterByNumber]
-        .forEach((filterFunction : (set: Set<MediaScan.TPN>, propertiesMap: Map<string, any>) => Set<MediaScan.TPN>, index) => {
+        .forEach((filterFunction: (set: Set<MediaScan.TPN>, propertiesMap: Map<string, any>) => Set<MediaScan.TPN>, index) => {
             result = filterFunction(result, propertiesWithAllProperties[index]);
         });
     return result;
@@ -105,19 +118,19 @@ export function filterTvSeriesByProperties(searchParameters: MediaScan.SearchPar
     } = mapProperties(searchParameters);
     const propertiesWithAllProperties
         = [booleanFieldsSearchMap, stringFieldsSearchMap, numberFieldsSearchMap];
-    let result : Map<string, Set<MediaScan.TPN>> = allTvSeries;
+    let result: Map<string, Set<MediaScan.TPN>> = allTvSeries;
     // filtering stuff
     // it also removes all entries that have an empty Set so that we can clearly see only valid things
 
     [filterByBoolean, filterByString, filterByNumber]
-        .forEach((filterFunction : (set: Set<MediaScan.TPN>, propertiesMap: Map<string, any>) => Set<MediaScan.TPN>, index) => {
+        .forEach((filterFunction: (set: Set<MediaScan.TPN>, propertiesMap: Map<string, any>) => Set<MediaScan.TPN>, index) => {
             result = new Map(<[string, Set<MediaScan.TPN>][]>
                 Array.from(
                     result.entries(),
                     ([showName, showSet]) => [showName, filterFunction(showSet, propertiesWithAllProperties[index])],
                 )
                 // eslint-disable-next-line no-unused-vars
-                .filter(([showName, showSet]) => (showSet as Set<MediaScan.TPN>).size > 0));
+                    .filter(([showName, showSet]) => (showSet as Set<MediaScan.TPN>).size > 0));
         });
 
     return result;
--- src/filters/filterStringProperty.ts
+++ src/filters/filterStringProperty.ts
@@ -1,32 +1,13 @@
 /** Provides a map with valid default properties */
 import MediaScan from "../declaration";
+import {filterDefaultProperties} from "../utils/utils_functions";
 
-const entries = require('object.entries');
-
-if (!Object.entries) {
-    entries.shim();
-}
-
-export function filterDefaultStringProperties(searchObject: MediaScan.DefaultSearchParameters): Map<string, string | string[]> {
+export function filterDefaultStringProperties(searchObject: MediaScan.DefaultSearchParameters): MediaScan.filterTuple<string|string[]>[] {
     const propertiesNames = ['title', 'resolution', 'codec', 'audio', 'group',
         'region', 'container', 'language', 'source'];
-    return Object.entries(searchObject).reduce((propertiesMap, [key, value]) => {
-        if (key in propertiesNames && (value !== undefined)) {
-            propertiesMap.set(key, value);
-        }
-        return propertiesMap;
-    }, new Map());
-}
-
-/** Remove the default string properties */
-export function excludeDefaultStringProperties(searchObject: MediaScan.DefaultSearchParameters): MediaScan.DefaultSearchParameters {
-    let rest = searchObject;
-    ['title', 'resolution', 'codec', 'audio', 'group',
-        'region', 'container', 'language', 'source'].forEach((propertyName) => {
-        if (propertyName in rest)
-            delete rest[propertyName];
+    return filterDefaultProperties<string|string[]>(propertiesNames, searchObject, (value) => {
+        return meetStringSpec(value);
     });
-    return rest;
 }
 
 /** Filter function for filterByString */
@@ -49,4 +30,12 @@ export function filterByString(set: Set<MediaScan.TPN>, propertiesMap: Map<strin
             (currentMoviesArray, val) => currentMoviesArray.filter(TPN => filterFunctionByType(val[0], val[1], TPN))
             , [...set],
         ));
+}
+
+// Just for type check this type
+export function meetStringSpec(value) {
+    if (Array.isArray(value))
+        return value.every( (elem) => (typeof elem === 'string') );
+    else
+        return (typeof value === 'string');
 }
